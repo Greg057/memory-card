b64e12d3739440e13bf63bcc5047f0e052968eaa
Greg057 <88341092+Greg057@users.noreply.github.com>
2024-01-28 11:41:35 +0100

    Get image pokemon with second api fetch

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,16 +3,31 @@ import { useState, useEffect } from "react"
 export default function Card () {
     const [pokemons, setPokemons] = useState()
 
+    function setUpPokemons (data) {
+        const pokemonsArray = data.map(async pokemon => {
+            const pokemonImage = await getPokemonImage(pokemon.url)
+            console.log(pokemonImage)
+            return {name: pokemon.name, image: pokemonImage}
+        })
+        setPokemons(pokemonsArray)
+        console.log(pokemons)
+    }
+
+    function getPokemonImage (url) { 
+        return new Promise(resolve => {
+            fetch(url)
+                .then(res => res.json())
+                .then(data => resolve(data.sprites.front_default))
+        })
+    }
+
     useEffect(() => {
         fetch("https://pokeapi.co/api/v2/pokemon/?limit=5")
             .then(res => res.json())
-            .then(data => fetch(data.results[0].url))
-            .then(res => res.json())
-            .then(data => setPokemons(data.sprites.front_default))
-            
+            .then(data => setUpPokemons(data.results))            
     }, [])
 
     return (
-        <img src={pokemons}/>
+        <p>hi</p>
     )
 }
