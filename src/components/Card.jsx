46b771c133cc67d4cde8bbcb9ce1784db7a9ae9d
Greg057 <88341092+Greg057@users.noreply.github.com>
2024-01-28 15:45:23 +0100

    Add logic to ensure random pokemon id is unique

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -8,36 +8,36 @@ export default function Card () {
 
     async function setUpPokemons (dataArr) {
         const newPokemon = await Promise.all(dataArr.map(async pokemon => {
-            const pokemonImage = await getPokemonImage(pokemon.url)
-            const id = pokemon.url[pokemon.url.length - 1] === "/" ? pokemon.url.slice(pokemon.url.length - 4).slice(0,3) : pokemon.url.slice(pokemon.url.length - 3)
-            console.log(id)
+            const pokemonImage = pokemon.sprites.front_default
+            const id = pokemon.game_indices[0].game_index
             let name = (pokemon.name.includes("-") ? pokemon.name.replace("-", " ") : pokemon.name)
             name = name[0].toUpperCase() + name.slice(1)
-            return {name: name, image: pokemonImage}
+            return {id: id, name: name, image: pokemonImage}
         }))
         setPokemons(newPokemon)
-    }
-
-    function getPokemonImage (url) { 
-        return new Promise((resolve, reject) => {
-            fetch(url)
-                .then(res => res.json())
-                .then(data => resolve(data.sprites.front_default))
-                .catch(err => reject(err))
-        })
     }
 
     useEffect(() => {
         async function fetchPokemons () {
             const dataArr = []
             const promises = [];
+            const IDs = []
             const totalNumberPokemons = 493 //until Generation IV included
            
                 for (let i = 0; i < NUMBER_POKEMONS; i++) {
-                    let id = Math.floor(Math.random() * totalNumberPokemons) + 1
-                    const promise = fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+                    let uniqueId = false
+                    let id
+                    while (uniqueId !== true) {
+                        id = Math.floor(Math.random() * totalNumberPokemons) + 1
+                        if (!IDs.find(idElement => idElement === id)) {
+                            uniqueId = true
+                            IDs.push(id)
+                        }
+                    }
+                    
+                    const promise = fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
                         .then(res => res.json())
-                        .then(data => dataArr.push(data.forms[0]))
+                        .then(data => dataArr.push(data))
                     
                         promises.push(promise)
                 }
